c66b0703cf7cd666b39abb75c53d59a23f455dbf
IgnacioSanhuezaZ <ignaciosanhuezaz@users.noreply.github.com>
2022-07-13 00:26:53 -0400

    Modulo base de datos en construcción, plantillas hechas para cada operación en DatabaseModule.py para la tabla Usuarios con selectUser, GetUser e insertUser. Se actualiza base de datos, nannys_pizza.sqlite .

--- Database/DatabaseModule.py
+++ Database/DatabaseModule.py
@@ -42,6 +42,14 @@ def Pragma_foreign_key_on(cursor):
 
 
 def selectUser(args, cursor, values=None):
+    """
+    Keys: Nombre_usuario, Contrasena, Administra
+
+    :param args:
+    :param cursor:
+    :param values:
+    :return:
+    """
     try:
         sqlite_select_query = """SELECT * from Usuarios """
         if args != "":
@@ -55,11 +63,147 @@ def selectUser(args, cursor, values=None):
     return records
 
 
+def selectClient(args, cursor, values=None):
+    """
+    Keys: Nombre_cliente, Direccion, Sector, Telefono
+    :param args:
+    :param cursor:
+    :param values:
+    :return:
+    """
+    try:
+        sqlite_select_query = """SELECT * from Cliente """
+        if args != "":
+            sqlite_select_query += args
+            records = list(cursor.execute(sqlite_select_query, tuple(values)))
+        else:
+            records = list(cursor.execute(sqlite_select_query))
+    except sqlite3.Error as error:
+        print("Failed to get data into sqlite table", error)
+        records = []
+    return records
+
+
+def selectBoletas(args, cursor, values=None):
+    """
+    Keys: Id_entrada, Id_boleta, Nombre_producto, Cantidad
+    :param args:
+    :param cursor:
+    :param values:
+    :return:
+    """
+    try:
+        sqlite_select_query = """SELECT * from Boletas """
+        if args != "":
+            sqlite_select_query += args
+            records = list(cursor.execute(sqlite_select_query, tuple(values)))
+        else:
+            records = list(cursor.execute(sqlite_select_query))
+    except sqlite3.Error as error:
+        print("Failed to get data into sqlite table", error)
+        records = []
+    return records
+
+
+def selectProducto(args, cursor, values=None):
+    """
+    Keys: Nombre_producto, Precio, Categoria
+    :param args:
+    :param cursor:
+    :param values:
+    :return:
+    """
+    try:
+        sqlite_select_query = """SELECT * from Producto """
+        if args != "":
+            sqlite_select_query += args
+            records = list(cursor.execute(sqlite_select_query, tuple(values)))
+        else:
+            records = list(cursor.execute(sqlite_select_query))
+    except sqlite3.Error as error:
+        print("Failed to get data into sqlite table", error)
+        records = []
+    return records
+
+
+def selectSesion(args, cursor, values=None):
+    """
+    Keys: Id_sesion, Nombre_usuario, Cerrado, Fecha_apertura, Fecha_cierre, Apertura_caja
+    :param args:
+    :param cursor:
+    :param values:
+    :return:
+    """
+    try:
+        sqlite_select_query = """SELECT * from Sesiones """
+        if args != "":
+            sqlite_select_query += args
+            records = list(cursor.execute(sqlite_select_query, tuple(values)))
+        else:
+            records = list(cursor.execute(sqlite_select_query))
+    except sqlite3.Error as error:
+        print("Failed to get data into sqlite table", error)
+        records = []
+    return records
+
+
+def selectTurnos_caja(args, cursor, values=None):
+    """
+    Keys: Id_turno, Id_sesion, Nombre_usuario
+    :param args:
+    :param cursor:
+    :param values:
+    :return:
+    """
+    try:
+        sqlite_select_query = """SELECT * from Turnos_caja """
+        if args != "":
+            sqlite_select_query += args
+            records = list(cursor.execute(sqlite_select_query, tuple(values)))
+        else:
+            records = list(cursor.execute(sqlite_select_query))
+    except sqlite3.Error as error:
+        print("Failed to get data into sqlite table", error)
+        records = []
+    return records
+
+
+def selectVentas(args, cursor, values=None):
+    """
+    Keys: Id_venta, Nombre_usuario, Id_boleta, Nombre_cliente, Direccion_cliente, Id_sesion
+    :param args:
+    :param cursor:
+    :param values:
+    :return:
+    """
+    try:
+        sqlite_select_query = """SELECT * from Ventas """
+        if args != "":
+            sqlite_select_query += args
+            records = list(cursor.execute(sqlite_select_query, tuple(values)))
+        else:
+            records = list(cursor.execute(sqlite_select_query))
+    except sqlite3.Error as error:
+        print("Failed to get data into sqlite table", error)
+        records = []
+    return records
+
+
+"""---------------------------------------------------------------------------------
+                                Obtención de datos
+   ---------------------------------------------------------------------------------"""
+
+
 def GetUser(user):
     cursor = get_db()
     record = selectUser("WHERE Nombre_usuario = ?;", cursor, (user,))
     cursor.close()
     return record
+
+
+"""---------------------------------------------------------------------------------
+                                Inserción de datos
+   ---------------------------------------------------------------------------------"""
 
 
 def insertUser(values, cursor):
@@ -70,3 +214,33 @@ def insertUser(values, cursor):
         print("Data inserted successfully into table User")
     except sqlite3.Error as error:
         print("Failed to insert data into table", error)
+
+
+"""---------------------------------------------------------------------------------
+                              Modificación de datos
+   ---------------------------------------------------------------------------------"""
+
+
+def updateUser(cursor, values):
+    """
+    Keys: Nombre_usuario, Contrasena, Administra
+
+    :param cursor: sqlite3 database cursor
+    :param values: tuple(Nombre_usuario, Contrasena, Administra, Nombre_usuario)
+    :return: None
+    """
+    query = """UPDATE Usuarios 
+                SET Nombre_usuario = ? ,
+                    Contrasena = ?,
+                    Administra = ?
+                WHERE Nombre_usuario = ?;"""
+    try:
+        if len(values) != 4:
+            print("Wrong amount of parameters!")
+            raise sqlite3.Error
+        else:
+            cursor.execute(query, tuple(values))
+            cursor.commit()
+            print("Table Usuarios successfully updated")
+    except sqlite3.Error as error:
+        print("Failed to update data from table", error)
